bc95149547342f5a6afe23e6261affde6561accc
Taha <taha.moriyama@gmail.com>
2020-07-23 12:55:27 +0900

    通信回りを記載

--- vibration-firm/vibration-firm.ino
+++ vibration-firm/vibration-firm.ino
@@ -30,11 +30,58 @@ void setup() {
   delay(50);
 }
 
+int i = 0;
+  //データ格納
+byte incomingbyte[8] = {0,0,0,0,0,0,0,0};
+byte incoming;
+
+byte incoming_s[8];
+byte finalout[8];
+
 void loop() {
-  byte buf = 0;
   // put your main code here, to run repeatedly:
+  
+  byte check_last = 0xFF;
   if (Serial.available() > 0) {
-    buf = Serial.read();
-    dacWrite(25, buf);
+    incoming = Serial.read();
+    if(check_last == incoming){
+      i = 0;
+    }else{
+      incomingbyte[i++] = incoming;
+    }
+  }
+  //Serial.printf("%d", i);
+
+   
+  for(int n = 0; n < 64; n++)
+  {
+    for(int s = 0; s < 8; s++)
+    {
+      if(incomingbyte[s] != 0)
+        {
+          incoming_s[s] = 0x01;
+                incomingbyte[s] = incomingbyte[s] - 1;
+        }else if(incoming_s[s] == 0){
+          incoming_s[s] = 0x00;  
+                incomingbyte[s] = incomingbyte[s] - 1;
+        }
+      finalout[0] = incoming_s[s];
+      if(s != 7) finalout[0] = finalout[0] << 1;  
+      //Serial.println(finalout[0]);
+    }
+  
+    for(int k = 0; k < 1; k++)
+    {
+      shift_dataOut(finalout[k]);  
+    }
+    strage_update();
+  }
+  for(int n = 0; n < 64; n++)
+  {
+    for(int k = 0; k < 1; k++)
+    {
+    shift_dataOut(0x00);
+    }  
+    strage_update();
   }
 }
